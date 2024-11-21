b218530be9496c8a5225e4371511fe7f8dcb2ab3
fantomas <iwsifatz@gmail.com>
2024-11-21 18:20:22 +0200

    "Added Swagger support to API, updated package.json and package-lock.json, and modified main.ts, create-user.dto.ts files"

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT=8000
+  const config = new DocumentBuilder()
+    .setTitle('Maestros api')
+    .setDescription('The maestros API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, documentFactory);
+
+  const PORT = 8000;
   await app.listen(PORT);
   app.useGlobalPipes(
     new ValidationPipe({
